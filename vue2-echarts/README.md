c2f53e04e6b18bedbed2321d1a297bdd7dac4f86
kanson5168 <1174195268@qq.com>
2024-07-11 07:45:48 +0800

    首次提交

---
+++ vue2-echarts/README.md
@@ -0,0 +1,30 @@
+# vue2-echarts
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run build
+npm run dev
+```
+
+### If you want to get the source code, please add wechat kanson5168
+
+```
+如果你想获取源码，请添加微信kanson5168
+```
+
+### Lints and fixes files
+
+```
+npm run lint
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ vue2-echarts/vue.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  lintOnSave: false,
+  devServer: {
+    host: "127.0.0.1"
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views',
+        'store': '@/store',
+        'styles': '@/styles'
+      }
+    }
+  }
+})
